fae55daa124cb7379e6f1ff90e3e76d1e1ac03dd
Bikrant Sahoo <bikrantsahoo@Bikrants-MacBook-Air.local>
2023-05-25 21:13:58 +0530

    added a change

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ def to_upper(name):
 
 def say_hello(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hello, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hello Bikrant, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    name = 'TrainWithShubham'
+    name = 'Bikrant is devops pro'
     say_hello(name)
     up = to_upper(name)
     print(up)
